f99c34054a20046e0ee09da6fd36b52e4677ca78
Marco Spinello <marco.s@miro.com>
2022-12-02 17:46:39 +0100

    refactor stuff

--- README.md
+++ README.md
@@ -8,14 +8,14 @@
 
 ### How to start locally
 
-- Run `npm i` to install dependencies.
+- Run `npm install` to install dependencies.
 - Run `npm start` to start developing. \
-  Your URL should be similar to this example:
- ```
- http://localhost:3000
- ```
+  Your URL should be similar to this example: \
+  ```
+  http://localhost:3000
+  ```
 - Paste the URL under **App URL** in your
-  [app settings](https://developers.miro.com/docs/build-your-first-hello-world-app#step-3-create-your-app-in-miro).
+  [app settings](https://developers.miro.com/docs/build-your-first-hello-world-app#step-2-create-your-app-in-miro).
 - Open a board; you should see your app in the app toolbar or in the **Apps**
   panel.
 
--- src/app.js
+++ src/app.js
@@ -1,48 +1,73 @@
 async function secretSanta () {
 
+  // Get the manually selected sticky notes.
   const stickyNotes = await miro.board.getSelection();
   console.log( stickyNotes );
-  let ids = stickyNotes.map( stickyNote => stickyNote.id );
-  console.log( ids );
 
-  const participants = buildCollection(ids);
+  // Create an empty collection of participants.
+  const participants = buildCollection( stickyNotes );
+  console.log( participants );
 
-  while ( Object.keys(participants).length > 1 ) {
-    const participantId1 = pickParticipant( participants );
-    const participantId2 = pickParticipant( participants );
+  // As long as there are at least 2 participants:
+  const numRemainingParticipants = Object.keys( participants ).length;
+  while ( numRemainingParticipants > 1 ) {
 
-    // create connection
+    // Pick a participant.
+    const participantId1 = pickAndRemoveParticipant( participants );
+
+    // Pick another participant.
+    const participantId2 = pickAndRemoveParticipant( participants );
+
+    // Create a connection between the 2 participants.
     await createConnection( participantId1, participantId2 );
     console.log( participants )
+
+    numRemainingParticipants = Object.keys( participants ).length;
   };
 };
 
-function buildCollection(ids) {
+function buildCollection(stickyNotes) {
+  // Set the collection as an empty object.
   let collection = {};
-  ids.forEach( id => {
-    collection[ id ] = id;
+  // { '123', 'Marco', '456', 'John', '789', 'Mary' }
+  // Add participant IDs to the collection.
+  stickyNotes.forEach( stickyNote => {
+    collection[ stickyNote.id ] = stickyNote.content;
   } );
   return collection;
 };
 
-function pickParticipant ( participants ) {
+// TODO: use splice to pick and remove participants IDs
+// create list of colors and connector terminals and randomize them.
+// create dedicated function to randomize getting the value of an index in an array.
+function pickAndRemoveParticipant ( participants ) {
+  // Get array of participant IDs.
   const ids = Object.keys( participants );
   console.log( ids );
-  const random = Math.floor(Math.random() * ids.length);
-//  console.log( random );
+  // Generate a random number between 0 and the number of participants.
+  const randomIndex = Math.floor( Math.random() * ids.length );
+  console.log( randomIndex );
 
-  const randomKey = ids[random];
-//  console.log( randomKey );
+  // Assign the random number to an index in the participants array.
+  // This selects a participant at random.
+  const randomId = ids[randomIndex];
+  console.log( randomId );
 
-  delete participants[ randomKey ];
-//  console.log( participants );
+  // Delete the randomly selected participant from the participants array.
+  delete participants[ randomId ];
+  console.log( participants );
   
-  return randomKey
+  return randomId
 }
 
+// Create a connector line between 2 selected participants.
 async function createConnection ( id1, id2 ) {
   await miro.board.createConnector( {
+
+    // Start at participant 1.
     start: { item: id1 },
+
+    // End at participant 2.
     end: { item: id2 },
   } );
 };
