0dbcbe41cf5a1dca3f52f3460e61818b1ca7aeb9
Marco Spinello <marco.s@miro.com>
2022-12-08 18:18:40 +0100

    add broken logic for connectors, sticky notes and randomization of styles

--- src/app.js
+++ src/app.js
@@ -22,6 +22,7 @@ async function secretSanta () {
     await createConnection( participantId1, participantId2 );
     console.log( participants )
 
+    // Update the number of remaining participants.
     numRemainingParticipants = Object.keys( participants ).length;
   };
 };
@@ -29,6 +30,7 @@ async function secretSanta () {
 function buildCollection(stickyNotes) {
   // Set the collection as an empty object.
   let collection = {};
+
   // { '123', 'Marco', '456', 'John', '789', 'Mary' }
   // Add participant IDs to the collection.
   stickyNotes.forEach( stickyNote => {
@@ -41,9 +43,11 @@ function buildCollection(stickyNotes) {
 // create list of colors and connector terminals and randomize them.
 // create dedicated function to randomize getting the value of an index in an array.
 function pickAndRemoveParticipant ( participants ) {
+
   // Get array of participant IDs.
   const ids = Object.keys( participants );
   console.log( ids );
+
   // Generate a random number between 0 and the number of participants.
   const randomIndex = Math.floor( Math.random() * ids.length );
   console.log( randomIndex );
@@ -56,9 +60,129 @@ function pickAndRemoveParticipant ( participants ) {
   // Delete the randomly selected participant from the participants array.
   delete participants[ randomId ];
   console.log( participants );
-  
-  return randomId
+
+  // Return the randomly selected ID of the participant.
+  return randomId;
 }
+
+// Enter number of participants
+async function participantNumber () {
+  // Prompt user to enter number of participants.
+  const howMany = prompt( "How many friends are participating in the Secret Santa?", "Enter a number between 2 and 20." );
+
+  // Convert the user input to an integer.
+  parseInt( howMany );
+  console.log( howMany );
+  
+  // Validate the user input.
+  if ( howMany === null || howMany === " " || howMany === NaN ) {
+    prompt( "That doesn't look like a number between 2 and 20. Try again.", "Enter a number between 2 and 20." );
+  } else if ( howMany < 2 ) {
+    prompt( "That's too few. Try again.", "Enter a number between 2 and 20" );
+  } else if ( howMany > 20 ) {
+    parseInt(prompt( "That's too many. Try again.", "Enter a number between 2 and 20" ));
+  } else {
+    alert( "Great! Click \"Enter their names\" to proceed." );
+  };
+
+  // Return the number of participants.
+  return howMany;
+};
+
+// Enter names of participants.
+async function participantNames () {
+ 
+  // Prompt user to enter names of participants.
+  const names = prompt( "What are the names of your friends? Enter a name for each participant.", "Names must be comma-separated." );
+  console.log( names );
+
+  // Validate the user input.
+  if ( names !== ([A-Za-z,]) ) {
+    prompt( "You can enter only alphabetic characters and commas. Try again.", "Names must be comma-separated." );
+  } else {
+    alert( "Great! Click \"Create sticky notes\" to proceed." );
+  };
+  
+  // Create an array of names.
+  const nameList = names.split( "," );
+  console.log( nameList );
+
+  // Trim leading and trailing spaces from each name.  
+  for (name in nameList) {
+    name = name.trim();
+  };
+
+  // Return the array of names.
+  return nameList;
+};
+
+const stickyNoteFillColor = [
+  gray,
+  light_yellow,
+  yellow,
+  orange,
+  light_green,
+  green,
+  dark_green,
+  cyan,
+  light_pink,
+  pink,
+  violet,
+  red,
+  light_blue,
+  blue,
+  dark_blue,
+  black
+];
+  
+const connectorStart = [
+  stealth,
+  arrow,
+  filled_triangle,
+  triangle,
+  filled_diamond,
+  diamond,
+  filled_oval,
+  oval,
+  erd_one,
+  erd_many,
+  erd_one_or_many,
+  erd_only_one,
+  erd_zero_or_many,
+  erd_zero_or_one
+];
+
+const connectorEnd = [
+  stealth,
+  arrow,
+  filled_triangle,
+  triangle,
+  filled_diamond,
+  diamond,
+  filled_oval,
+  oval,
+  erd_one,
+  erd_many,
+  erd_one_or_many,
+  erd_only_one,
+  erd_zero_or_many,
+  erd_zero_or_one
+];
+
+const connectorColor = Math.floor( Math.random() * 16777215 ).toString( 16 );
+console.log( connectorColor );
+
+// Create a sticky note for each participant.
+async function createStickyNotes ( howMany, nameList[i] ) {
+  await miro.board.createStickyNote({
+    content: nameList[i],
+    style: {
+      fillColor: randomFillColor,
+    },
+    x: 4500,
+    y: 4500,
+  });
+};
 
 // Create a connector line between 2 selected participants.
 async function createConnection ( id1, id2 ) {
@@ -69,6 +193,11 @@ async function createConnection ( id1, id2 ) {
 
     // End at participant 2.
     end: { item: id2 },
+
+    startStrokeCap: connectorRandomStartStrokeCap,
+    endStrokeCap: connectorRandomEndStrokeCap,
+    strokeColor: connectorRandomStrokeColor,
+
   } );
 };
 
